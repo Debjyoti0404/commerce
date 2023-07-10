9f32e900822af325224a70d6c5c4307487749216
Debjyoti0404 <debsen2003@gmail.com>
2023-07-10 16:42:05 +0530

    registered models in the admin section

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class User(AbstractUser):
     pass
 
-class auction_items(models.Model):
+class AuctionItems(models.Model):
     prdct_name = models.CharField(max_length=64)
     prdct_desc = models.CharField(max_length=200)
     prdct_price = models.FloatField()
--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -4,11 +4,14 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import User, AuctionItems
 
 
 def index(request):
-    return render(request, "auctions/index.html")
+    all_auctionitem = AuctionItems.objects.all()
+    return render(request, "auctions/index.html", {
+        "all_auctionitems" : all_auctionitem
+    })
 
 
 def login_view(request):
