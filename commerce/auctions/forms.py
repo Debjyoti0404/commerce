fdc8ad18db6fb7e460f09222caaf2af628ba8eef
Debjyoti0404 <debsen2003@gmail.com>
2024-03-23 17:18:58 +0530

    added bit of ui changes

--- commerce/auctions/forms.py
+++ commerce/auctions/forms.py
@@ -7,8 +7,28 @@ class CommentForm(forms.Form):
     comment = forms.CharField(label="Comment", max_length=300)
 
 class ProductForm(forms.Form):
-    prdct_name = forms.CharField(label="Product Name", max_length=64)
-    prdct_desc = forms.CharField(label="Description", max_length=500)
-    prdct_price = forms.FloatField(label="Initial Price")
-    prdct_img = forms.URLField(label="Product Picture")
-    prdct_category = forms.CharField(label="Category", max_length=64)
+    prdct_name = forms.CharField(label="Product Name", widget=forms.Textarea(attrs={
+        "class":"form-control border border-primary mb-2",
+        "type":"text",
+        "rows":"1"
+    }), max_length=64)
+    prdct_desc = forms.CharField(label="Description", widget=forms.Textarea(attrs={
+        "class":"form-control border border-primary mb-2",
+        "type":"text",
+        "rows":"1"
+    }), max_length=500)
+    prdct_price = forms.FloatField(label="Initial Price", widget=forms.Textarea(attrs={
+        "class":"form-control border border-primary mb-2",
+        "type":"text",
+        "rows":"1"
+    }))
+    prdct_img = forms.URLField(label="Product Picture", widget=forms.Textarea(attrs={
+        "class":"form-control border border-primary mb-2",
+        "type":"text",
+        "rows":"1"
+    }))
+    prdct_category = forms.CharField(label="Category", widget=forms.Textarea(attrs={
+        "class":"form-control border border-primary mb-2",
+        "type":"text",
+        "rows":"1"
+    }), max_length=64)
